f3e49c899cebb89b5ce82287d869e5a8c9bc3564
ark21yan <120375741+ark21yan@users.noreply.github.com>
2023-01-31 10:38:59 +0000

    Pending changes exported from your codespace

---
+++ src/views/Index.vue
@@ -0,0 +1,68 @@
+<template>
+    <section class="about">
+            <h1 class="color-red">О нас:</h1>
+            <p class="font">Инофрмация о скрвисе Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut magnam ipsum tempore omnis perferendis consectetur veniam similique nesciunt optio, suscipit non facilis ea! Laborum ex voluptatibus ut alias quasi omnis!</p>
+            <p class="font">Описание предоставлеемых услуг Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto officia iste commodi facilis omnis ab sunt voluptates iure ex temporibus!</p>
+            <button class="btn btn-primary">Запись на консультацию</button>
+        </section>
+        <section class="konsultants">
+            <h2 class="color-red g-full">Консультанты:</h2>
+            <div class="item" v-for="consultant in consultants">
+                <img src="../assets/img/IMAGE-1.png" alt="">
+                <h3 class="color-red">{{consultant.firstname}}</h3>
+                <p class="font">Описание Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur debitis blanditiis repudiandae ea fugit ratione ducimus. Laborum consequatur ea voluptatem.</p>
+            </div>            
+        </section>
+        <section class="galerey">
+            <h2 class="color-red g-full">Галерея:</h2>
+             <div class="item">
+                <img src="../assets/img/IMAGE-3.png" alt="">
+             </div>
+             <div class="item">
+                <img src="../assets/img/IMAGE-4.png" alt="">
+             </div>
+             <div class="item">
+                <img src="../assets/img/IMAGE-5.png" alt="">
+             </div>
+             <div class="item">
+                <img src="../assets/img/IMAGE-6.png" alt="">
+             </div>
+        </section>
+        <section class="otsivs">
+            <h2 class="color-red g-full">Отзывы:</h2>
+            <div class="item">
+                <h4 class="color-red">Имя</h4>
+                <p class="font">отзыв Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae, velit voluptatibus at odio a dolores incidunt eius repudiandae ipsum quibusdam ratione. Ipsam, sunt architecto! Rem mollitia repudiandae fugiat aspernatur doloremque!</p>
+            </div>
+            <div class="item">
+                <h4 class="color-red">Имя</h4>
+                <p class="font">отзыв Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima perspiciatis non eum quaerat hic, cupiditate iste velit magnam consequatur at mollitia itaque ab ipsam iure suscipit error dolor aperiam, laboriosam dolore. Debitis quia rem repudiandae incidunt, quae reprehenderit iste sit.</p>
+            </div>
+            <div class="item">
+                <h4 class="color-red">Имя</h4>
+                <p class="font">отзыв Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae, velit voluptatibus at odio a dolores incidunt eius repudiandae ipsum quibusdam ratione. Ipsam, sunt architecto! Rem mollitia repudiandae fugiat aspernatur doloremque!</p>
+            </div>
+        </section>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+    let consultants = ref([]);
+
+     axios({
+        method:"get",
+        url:"http://localhost/fedorovRestApi/public/api/regions/1/organizations/CEMK/consultants",
+        headers:{
+            Authorization: `Bearer ${localStorage.token}`,
+        }
+    }).then((res)=>{
+        consultants.value = res.data.consultants        
+    })
+
+</script>
+
+<style >
+
+</style>
---
+++ src/views/Record.vue
@@ -0,0 +1,86 @@
+<template>
+    <section class="">
+            <h2 class="color-red g-full text-center" >Запись на консультацию:</h2>
+            <form class="form-primary" action="#" @submit.prevent="addBid">
+                <label class="label-primary" for= "">ФИО:</label>
+                <input class="input-primary" v-model="forms.fio" type="text">               
+                <label class="label-primary" for= "">Электронная почта:</label>
+                <input class="input-primary" v-model="forms.email" type="email">
+                <label class="label-primary" for= "">Телефон :</label>
+                <input class="input-primary" v-model="forms.phone" type="tel">
+                <label class="label-primary" for= "">ФИО ребенка :</label>
+                <input class="input-primary" v-model="forms.chfio" type="text">                
+                <label class="label-primary" for= "">Возраст ребенка :</label>
+                <select class="input-primary" v-model="forms.age">
+                    <option v-for="n in 18">{{n}}</option>                    
+                </select>
+                <label class="label-primary" for= "">Регион проживания :</label>
+                <input class="input-primary" v-model="forms.region" type="text">
+                <label class="label-primary" for= "">Образовательная организация :</label>
+                <input class="input-primary" v-model="forms.organization" type="text">
+                <label class="label-primary" for= "">Категория вопроса:</label>
+                <select class="input-primary" v-model="forms.category">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <label class="label-primary" for="">Описание вопроса:</label>
+                <textarea class="input-primary" rows="10" v-model="forms.question"></textarea>
+                <label class="label-primary" for= "">Консультант:</label>
+                <input class="input-primary" v-model="forms.consultant" type="text">
+                <label class="label-primary" for= "">Дата записи:</label>
+                <input class="input-primary" v-model="forms.date" type="text">
+                <button class="btn btn-primary">Отправить заявку</button>
+            </form>
+        </section>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+    let forms = ref({});
+    forms.value.fio= "asd asdas asd"
+    forms.value.email = "example@mail.ru"
+    forms.value.phone = "sdasd"
+    forms.value.chfio = "asdasd a as "
+    forms.value.age = "14"
+    forms.value.region = "1"
+    forms.value.organization = "1"
+    forms.value.consultant = "1"
+    forms.value.category = "1"
+    forms.value.question = "1"
+    forms.value.date = "23.10.10"
+
+    const addBid = async()=>{
+        await axios({
+        method:"post",
+        url:"http://localhost/fedorovRestApi/public/api/consultations",
+        headers:{
+            Authorization: `Bearer ${localStorage.token}`,
+        },
+        data:{
+            fio:forms.value.fio,
+            email:forms.value.email,
+            phone:forms.value.phone,
+            chfio:forms.value.chfio,
+            age:forms.value.age,
+            region_id:forms.value.region,
+            organization_id:forms.value.organization,
+            consultant_id:forms.value.consultant,
+            category_question_id:forms.value.category,
+            question_id:forms.value.question,
+            date:forms.value.date,
+        },
+        }).then((res)=>{
+            console.log(res)
+            alert(res.data.consultation.code)  
+        }).catch((err)=>{
+            alert("ghjdfktyj")  
+            console.log(err)
+        })
+    }
+</script>
+
